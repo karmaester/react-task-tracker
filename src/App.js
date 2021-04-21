9d5c142ffef961130616dbe7631f92631f514f9b
Khristian Rojas <khristianrojas15@gmail.com>
2021-04-21 12:17:21 -0400

    Add showAdd state and

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -27,7 +28,10 @@ function App() {
 
 //Add Task
 const addTask = (task) => {
-  console.log(task)
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+
 }
 
 //Delete Task
@@ -45,8 +49,9 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} 
+      showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete=
         {deleteTask} onToggle={toggleReminder} />
